aa5f1570b366907d03694cab66a7fbab6544fb45
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-19 17:19:37 +0100

    19.03

--- 02-mouse-events/script.js
+++ 02-mouse-events/script.js
@@ -3,28 +3,34 @@
 const logo = document.querySelector('img');
 
 const onClick = () => console.log('Click event');
-const onDoubleClick = () => console.log('Double click event');
-const onRightMouse = () => console.log('Right click event');
-const onMouseDown = () => console.log('Mouse down event');
+const onDoubleClick = () => {
+	if (document.body.style.backgroundColor !== 'purple') {
+		document.body.style.backgroundColor = 'purple';
+		document.body.style.color = 'white';
+	} else {
+		document.body.style.backgroundColor = 'white';
+		document.body.style.color = 'black';
+	}
+};
+const onRightClick = () => console.log('Right click event');
+const onMouseDown = () => console.log('Mouse down');
 const onMouseUp = () => console.log('Mouse up');
 const onWheel = () => console.log('Wheel');
 const onMouseOver = () => console.log('Mouse Over');
 const onMouseOut = () => console.log('Mouse Out');
-const onDragStart = () => console.log('Drag Start Event');
-const onDrag = () => console.log('Drag Event');
-const onDragEnd = () => console.log('Drag End Event');
+const onDragStart = () => console.log('Drag Start');
+const onDragEnd = () => console.log('Drag End');
+const onDrag = () => console.log('Drag');
 
-// Button
+// Event Listeners
 logo.addEventListener('click', onClick);
 logo.addEventListener('dblclick', onDoubleClick);
-logo.addEventListener('contextmenu', onRightMouse);
+logo.addEventListener('contextmenu', onRightClick);
 logo.addEventListener('mousedown', onMouseDown);
 logo.addEventListener('mouseup', onMouseUp);
 logo.addEventListener('wheel', onWheel);
-// Hover
-logo.addEventListener('mousemove', onMouseOver);
+logo.addEventListener('mouseover', onMouseOver);
 logo.addEventListener('mouseout', onMouseOut);
-// Drag
 logo.addEventListener('dragstart', onDragStart);
 logo.addEventListener('drag', onDrag);
 logo.addEventListener('dragend', onDragEnd);
