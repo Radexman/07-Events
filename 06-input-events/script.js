f87f0e9905df13bec65d39c3f8a6989177739fb5
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-20 22:32:26 +0100

    20.03

--- 06-input-events/script.js
+++ 06-input-events/script.js
@@ -1,29 +1,28 @@
 // ============= 06 Input Events ============= //
 
-const inputElement = document.querySelector('#item-input');
-const priorityInput = document.getElementById('priority-input');
+const itemInput = document.querySelector('#item-input');
+const priorityInput = document.querySelector('#priority-input');
 const checkbox = document.querySelector('#checkbox');
+const heading = document.querySelector('h1');
 
-function onInput(e) {
-	const h1 = document.querySelector('h1');
-	h1.innerText = e.target.value;
-}
+const onInput = (e) => {
+	heading.textContent = e.target.value;
+};
 
 const onCheck = (e) => {
-	const h1 = document.querySelector('h1');
-	h1.textContent = checkbox.checked ? 'Checked' : 'Unchecked';
+	heading.textContent = checkbox.checked ? 'Checked' : 'Unchecked';
 };
 
 const onFocus = (e) => {
-	inputElement.classList.add('focus');
+	itemInput.style.outline = '1px solid green';
 };
 
 const onBlur = (e) => {
-	inputElement.classList.remove('focus');
+	itemInput.style.outline = 'none';
 };
 
-// inputElement.addEventListener('input', onKeyDown);
+itemInput.addEventListener('input', onInput);
 priorityInput.addEventListener('input', onInput);
 checkbox.addEventListener('input', onCheck);
-inputElement.addEventListener('focus', onFocus);
-inputElement.addEventListener('blur', onBlur);
+itemInput.addEventListener('focus', onFocus);
+itemInput.addEventListener('blur', onBlur);
